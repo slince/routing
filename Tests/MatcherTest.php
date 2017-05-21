ffaf2c2fc40b47d5c68f55ee69abd8112d46b68b
slince <taosikai@yeah.net>
2017-05-21 20:09:09 +0800

    fixtestcase

--- Tests/MatcherTest.php
+++ Tests/MatcherTest.php
@@ -79,7 +79,7 @@ class MatcherTest extends TestCase
         }
     }
 
-    public function testMatchWithPrefixes()
+    public function testMatchWithPrefix()
     {
         $routes = new RouteCollection();
         $routes->group('foo', function(RouteCollection $routes){
@@ -103,214 +103,211 @@ class MatcherTest extends TestCase
     {
         $routes = new RouteCollection();
         $routes->http('/foo', 'action1')->setName('$péß^a|');
-        $matcher = new UrlMatcher($routesection, new RequestContext());
-        $this->assertEquals(array('_route' => '$péß^a|'), $matcher->match('/bar'));
+        $matcher = new Matcher($routes);
+        $this->assertEquals('$péß^a|', $matcher->match('/foo')->getName());
     }
+
     public function testMatchNonAlpha()
     {
-        $routesection = new RouteCollection();
+        $routes = new RouteCollection();
         $chars = '!"$%éà &\'()*+,./:;<=>@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\[]^_`abcdefghijklmnopqrstuvwxyz{|}~-';
-        $routesection->add('foo', new Route('/{foo}/bar', array(), array('foo' => '['.preg_quote($chars).']+'), array('utf8' => true)));
-        $matcher = new UrlMatcher($routesection, new RequestContext());
-        $this->assertEquals(array('_route' => 'foo', 'foo' => $chars), $matcher->match('/'.rawurlencode($chars).'/bar'));
-        $this->assertEquals(array('_route' => 'foo', 'foo' => $chars), $matcher->match('/'.strtr($chars, array('%' => '%25')).'/bar'));
+        $routes->http('/{foo}/bar', 'action')
+            ->setRequirements([
+                'foo' => '['.preg_quote($chars).']+'
+            ]);
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['foo' => $chars], $matcher->match('/'.rawurlencode($chars).'/bar')->getComputedParameters());
+        $this->assertEquals(['foo' => $chars], $matcher->match('/'.strtr($chars, array('%' => '%25')).'/bar')->getComputedParameters());
     }
+
     public function testMatchWithDotMetacharacterInRequirements()
     {
-        $routesection = new RouteCollection();
-        $routesection->add('foo', new Route('/{foo}/bar', array(), array('foo' => '.+')));
-        $matcher = new UrlMatcher($routesection, new RequestContext());
-        $this->assertEquals(array('_route' => 'foo', 'foo' => "\n"), $matcher->match('/'.urlencode("\n").'/bar'), 'linefeed character is matched');
+        $routes = new RouteCollection();
+        $routes->http('/{foo}/bar', 'action')
+            ->setRequirements(['foo' => '.+']);
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['foo' => "\n"], $matcher->match('/'.urlencode("\n").'/bar')->getComputedParameters());
     }
-    public function testMatchOverriddenRoute()
-    {
-        $routesection = new RouteCollection();
-        $routesection->add('foo', new Route('/foo'));
-        $routesection1 = new RouteCollection();
-        $routesection1->add('foo', new Route('/foo1'));
-        $routesection->addCollection($routesection1);
-        $matcher = new UrlMatcher($routesection, new RequestContext());
-        $this->assertEquals(array('_route' => 'foo'), $matcher->match('/foo1'));
-        $this->{method_exists($this, $_ = 'expectException') ? $_ : 'setExpectedException'}('Symfony\Component\Routing\Exception\ResourceNotFoundException');
-        $this->assertEquals(array(), $matcher->match('/foo'));
-    }
+
     public function testMatchRegression()
     {
         $routes = new RouteCollection();
-        $routes->add('foo', new Route('/foo/{foo}'));
-        $routes->add('bar', new Route('/foo/bar/{foo}'));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('foo' => 'bar', '_route' => 'bar'), $matcher->match('/foo/bar/bar'));
-        $routesection = new RouteCollection();
-        $routesection->add('foo', new Route('/{bar}'));
-        $matcher = new UrlMatcher($routesection, new RequestContext());
-        try {
-            $matcher->match('/');
-            $this->fail();
-        } catch (ResourceNotFoundException $e) {
-        }
+        $routes->http('/foo/{foo}', 'action');
+        $routes->http('/foo/bar/{foo}', 'action2');
+
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['foo' => 'bar'], $matcher->match('/foo/bar/bar')->getComputedParameters());
+
+        $routes = new RouteCollection();
+        $routes->http('/{bar}', 'action');
+        $matcher = new Matcher($routes);
+        $this->expectException(RouteNotFoundException::class);
+        $matcher->match('/');
     }
+
     public function testDefaultRequirementForOptionalVariables()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/{page}.{_format}', array('page' => 'index', '_format' => 'html')));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('page' => 'my-page', '_format' => 'xml', '_route' => 'test'), $matcher->match('/my-page.xml'));
+        $routes->http('/{page}.{_format}', 'action')->setDefaults([
+            '_format' => 'html'
+        ]);
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['page' => 'my-page', '_format' => 'xml'], $matcher->match('/personal-page.xml')
+            ->getComputedParameters());
     }
+
     public function testMatchingIsEager()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/{foo}-{bar}-', array(), array('foo' => '.+', 'bar' => '.+')));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('foo' => 'text1-text2-text3', 'bar' => 'text4', '_route' => 'test'), $matcher->match('/text1-text2-text3-text4-'));
+        $routes->http('/{foo}-{bar}-', 'action')
+            ->setRequirements([
+                'foo' => '.+',
+                'bar' => '.+'
+            ]);
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['foo' => 'text1-text2-text3', 'bar' => 'text4'],
+            $matcher->match('/text1-text2-text3-text4-')->getComputedParameters());
     }
+
     public function testAdjacentVariables()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/{w}{x}{y}{z}.{_format}', array('z' => 'default-z', '_format' => 'html'), array('y' => 'y|Y')));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        // 'w' eagerly matches as much as possible and the other variables match the remaining chars.
-        // This also shows that the variables w-z must all exclude the separating char (the dot '.' in this case) by default requirement.
-        // Otherwise they would also consume '.xml' and _format would never match as it's an optional variable.
-        $this->assertEquals(array('w' => 'wwwww', 'x' => 'x', 'y' => 'Y', 'z' => 'Z', '_format' => 'xml', '_route' => 'test'), $matcher->match('/wwwwwxYZ.xml'));
+        $routes->http('/{w}{x}{y}{z}.{_format}', 'action')
+            ->setDefaults(['z' => 'default-z', '_format' => 'html'])
+            ->setRequirements(['y' => 'y|Y']);
+
+        $matcher = new Matcher($routes);
+        $this->assertEquals(array('w' => 'wwwww', 'x' => 'x', 'y' => 'Y', 'z' => 'Z', '_format' => 'xml'),
+            $matcher->match('/wwwwwxYZ.xml')->getComputedParameters());
+
         // As 'y' has custom requirement and can only be of value 'y|Y', it will leave  'ZZZ' to variable z.
         // So with carefully chosen requirements adjacent variables, can be useful.
-        $this->assertEquals(array('w' => 'wwwww', 'x' => 'x', 'y' => 'y', 'z' => 'ZZZ', '_format' => 'html', '_route' => 'test'), $matcher->match('/wwwwwxyZZZ'));
+        $this->assertEquals(array('w' => 'wwwww', 'x' => 'x', 'y' => 'y', 'z' => 'ZZZ', '_format' => 'html'),
+            $matcher->match('/wwwwwxyZZZ')->getComputedParameters());
+
         // z and _format are optional.
-        $this->assertEquals(array('w' => 'wwwww', 'x' => 'x', 'y' => 'y', 'z' => 'default-z', '_format' => 'html', '_route' => 'test'), $matcher->match('/wwwwwxy'));
-        $this->{method_exists($this, $_ = 'expectException') ? $_ : 'setExpectedException'}('Symfony\Component\Routing\Exception\ResourceNotFoundException');
+        $this->assertEquals(array('w' => 'wwwww', 'x' => 'x', 'y' => 'y', 'z' => 'default-z', '_format' => 'html'),
+            $matcher->match('/wwwwwxy')->getComputedParameters());
+        $this->expectException(RouteNotFoundException::class);
         $matcher->match('/wxy.html');
     }
+
     public function testOptionalVariableWithNoRealSeparator()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/get{what}', array('what' => 'All')));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('what' => 'All', '_route' => 'test'), $matcher->match('/get'));
-        $this->assertEquals(array('what' => 'Sites', '_route' => 'test'), $matcher->match('/getSites'));
-        // Usually the character in front of an optional parameter can be left out, e.g. with pattern '/get/{what}' just '/get' would match.
-        // But here the 't' in 'get' is not a separating character, so it makes no sense to match without it.
-        $this->{method_exists($this, $_ = 'expectException') ? $_ : 'setExpectedException'}('Symfony\Component\Routing\Exception\ResourceNotFoundException');
+        $routes->http('/get{what}', 'action')
+            ->setDefaults(['what' => 'All']);
+        $matcher = new Matcher($routes);
+
+        $this->assertEquals(['what' => 'All'], $matcher->match('/get')->getComputedParameters());
+        $this->assertEquals(['what' => 'Sites'], $matcher->match('/getSites')->getComputedParameters());
+        $this->expectException(RouteNotFoundException::class);
         $matcher->match('/ge');
     }
+
     public function testRequiredVariableWithNoRealSeparator()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/get{what}Suffix'));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('what' => 'Sites', '_route' => 'test'), $matcher->match('/getSitesSuffix'));
+        $routes->http('/get{what}Suffix', 'action');
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['what' => 'Sites'], $matcher->match('/getSitesSuffix')->getComputedParameters());
     }
+
     public function testDefaultRequirementOfVariable()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/{page}.{_format}'));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('page' => 'index', '_format' => 'mobile.html', '_route' => 'test'), $matcher->match('/index.mobile.html'));
+        $routes->http('/{page}.{_format}', 'action');
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['page' => 'index', '_format' => 'mobile.html'],
+            $matcher->match('/index.mobile.html')->getComputedParameters());
     }
-    /**
-     * @expectedException \Symfony\Component\Routing\Exception\ResourceNotFoundException
-     */
+
     public function testDefaultRequirementOfVariableDisallowsSlash()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/{page}.{_format}'));
-        $matcher = new UrlMatcher($routes, new RequestContext());
+        $routes->http('/{page}.{_format}', 'action');
+        $matcher = new Matcher($routes);
+        $this->expectException(RouteNotFoundException::class);
         $matcher->match('/index.sl/ash');
     }
-    /**
-     * @expectedException \Symfony\Component\Routing\Exception\ResourceNotFoundException
-     */
+
     public function testDefaultRequirementOfVariableDisallowsNextSeparator()
     {
         $routes = new RouteCollection();
-        $routes->add('test', new Route('/{page}.{_format}', array(), array('_format' => 'html|xml')));
-        $matcher = new UrlMatcher($routes, new RequestContext());
+        $routes->http('/{page}.{_format}', 'action')
+            ->setDefaults(['_format' => 'html|xml']);
+        $matcher = new Matcher($routes);
+        $this->expectException(RouteNotFoundException::class);
         $matcher->match('/do.t.html');
     }
-    /**
-     * @expectedException \Symfony\Component\Routing\Exception\ResourceNotFoundException
-     */
+
     public function testSchemeRequirement()
     {
         $routes = new RouteCollection();
-        $routes->add('foo', new Route('/foo', array(), array(), array(), '', array('https')));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $matcher->match('/foo');
+        $routes->https('/foo', 'action')
+            ->setDefaults(['_format' => 'html|xml']);
+        $matcher = new Matcher($routes);
+        $this->expectException(RouteNotFoundException::class);
+        $request = new ServerRequest([], [], 'http://www.domain.com/foo');
+        $matcher->matchRequest($request);
     }
-    /**
-     * @expectedException \Symfony\Component\Routing\Exception\ResourceNotFoundException
-     */
+
     public function testCondition()
     {
         $routes = new RouteCollection();
-        $route = new Route('/foo');
-        $route->setCondition('context.getMethod() == "POST"');
-        $routes->add('foo', $route);
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $matcher->match('/foo');
+        $routes->post('/foo', 'action')
+            ->setDefaults(['_format' => 'html|xml']);
+        $matcher = new Matcher($routes);
+        $this->expectException(RouteNotFoundException::class);
+        $request = new ServerRequest(['HTTP_REQUEST_METHOD' => 'post'], [], 'http://www.domain.com/foo');
+        $matcher->matchRequest($request);
     }
+
     public function testDecodeOnce()
     {
         $routes = new RouteCollection();
-        $routes->add('foo', new Route('/foo/{foo}'));
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('foo' => 'bar%23', '_route' => 'foo'), $matcher->match('/foo/bar%2523'));
+        $routes->http('/foo/{foo}', 'action');
+        $matcher = new Matcher($routes);
+        $this->assertEquals(['foo' => 'bar%23'], $matcher->match('/foo/bar%2523')->getComputedParameters());
     }
-    public function testCannotRelyOnPrefix()
-    {
-        $routes = new RouteCollection();
-        $subColl = new RouteCollection();
-        $subColl->add('bar', new Route('/bar'));
-        $subColl->addPrefix('/prefix');
-        // overwrite the pattern, so the prefix is not valid anymore for this route in the collection
-        $subColl->get('bar')->setPath('/new');
-        $routes->addCollection($subColl);
-        $matcher = new UrlMatcher($routes, new RequestContext());
-        $this->assertEquals(array('_route' => 'bar'), $matcher->match('/new'));
-    }
+
     public function testWithHost()
     {
         $routes = new RouteCollection();
-        $routes->add('foo', new Route('/foo/{foo}', array(), array(), array(), '{locale}.example.com'));
-        $matcher = new UrlMatcher($routes, new RequestContext('', 'GET', 'en.example.com'));
-        $this->assertEquals(array('foo' => 'bar', '_route' => 'foo', 'locale' => 'en'), $matcher->match('/foo/bar'));
+        $routes->http('/foo/{foo}', 'action')->setHost('{locale}.example.com');
+
+        $matcher = new Matcher($routes);
+        $request = new ServerRequest(['HTTP_REQUEST_METHOD' => 'post'], [], 'http:// en.example.com/foo/bar');
+        $this->assertEquals(['foo' => 'bar', 'locale' => 'en'], $matcher->matchRequest($request));
     }
-    public function testWithHostOnRouteCollection()
-    {
-        $routes = new RouteCollection();
-        $routes->add('foo', new Route('/foo/{foo}'));
-        $routes->add('bar', new Route('/bar/{foo}', array(), array(), array(), '{locale}.example.net'));
-        $routes->setHost('{locale}.example.com');
-        $matcher = new UrlMatcher($routes, new RequestContext('', 'GET', 'en.example.com'));
-        $this->assertEquals(array('foo' => 'bar', '_route' => 'foo', 'locale' => 'en'), $matcher->match('/foo/bar'));
-        $matcher = new UrlMatcher($routes, new RequestContext('', 'GET', 'en.example.com'));
-        $this->assertEquals(array('foo' => 'bar', '_route' => 'bar', 'locale' => 'en'), $matcher->match('/bar/bar'));
-    }
-    /**
-     * @expectedException \Symfony\Component\Routing\Exception\ResourceNotFoundException
-     */
+
     public function testWithOutHostHostDoesNotMatch()
     {
         $routes = new RouteCollection();
-        $routes->add('foo', new Route('/foo/{foo}', array(), array(), array(), '{locale}.example.com'));
-        $matcher = new UrlMatcher($routes, new RequestContext('', 'GET', 'example.com'));
-        $matcher->match('/foo/bar');
+        $routes->http('/foo/{foo}', 'action')->setHost('{locale}.example.com');
+        $matcher = new Matcher($routes);
+        $request = new ServerRequest(['HTTP_REQUEST_METHOD' => 'post'], [], 'http://example.com/foo/bar');
+        $this->expectException(RouteNotFoundException::class);
+        $matcher->matchRequest($request);
     }
-    /**
-     * @expectedException \Symfony\Component\Routing\Exception\ResourceNotFoundException
-     */
+
     public function testPathIsCaseSensitive()
     {
         $routes = new RouteCollection();
-        $routes->add('foo', new Route('/locale', array(), array('locale' => 'EN|FR|DE')));
-        $matcher = new UrlMatcher($routes, new RequestContext());
+        $routes->http('/locale','action')->setRequirements(['locale' => 'EN|FR|DE']);
+        $matcher = new Matcher($routes);
+        $this->expectException(RouteNotFoundException::class);
         $matcher->match('/en');
     }
+
     public function testHostIsCaseInsensitive()
     {
         $routes = new RouteCollection();
-        $routes->add('foo', new Route('/', array(), array('locale' => 'EN|FR|DE'), array(), '{locale}.example.com'));
-        $matcher = new UrlMatcher($routes, new RequestContext('', 'GET', 'en.example.com'));
-        $this->assertEquals(array('_route' => 'foo', 'locale' => 'en'), $matcher->match('/'));
+        $routes->http('/','action')->setRequirements(['locale' => 'EN|FR|DE'])
+            ->setHost('{locale}.example.com');
+        $matcher = new Matcher($routes);
+        $this->expectException(RouteNotFoundException::class);
+        $request = new ServerRequest(['HTTP_REQUEST_METHOD' => 'post'], [], 'http://en.example.com/');
+        $this->assertEquals(array('locale' => 'en'), $matcher->matchRequest($request)->getComputedParameters());
     }
 }
